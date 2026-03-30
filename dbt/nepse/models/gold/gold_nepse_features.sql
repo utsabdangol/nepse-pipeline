d5d146d8dd96e993b1c72f5af746194f159850bf
utsabdangol <107794952+utsabdangol@users.noreply.github.com>
2026-03-30 13:22:50 +0545

    feat: add dbt tests for silver and gold models

--- dbt/nepse/models/gold/gold_nepse_features.sql
+++ dbt/nepse/models/gold/gold_nepse_features.sql
@@ -4,6 +4,10 @@ SELECT
     ltp,
     price_change,
     change_pct,
+    -- calculates the 7-day average price for each symbol using a window function
+    -- partion is used to group stocks
+    -- order by is used to ensure the average is calculated in the correct order of dates
+    -- row preceding and current row is used to specify the range of rows to include in the average calculation (7 days)
     AVG(ltp) OVER (
         PARTITION BY symbol
         ORDER BY scraped_date
@@ -14,6 +18,7 @@ SELECT
         ORDER BY scraped_date
         ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
     ) AS avg_30_day,
+    -- calculates the 30-day maximum price for each symbol using a window function
     MAX(ltp) OVER (
         PARTITION BY symbol
         ORDER BY scraped_date
